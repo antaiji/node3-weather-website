79d157f8428d291331c3479e576687e605a8b329
Asahi Daigo <antaiji@yandex.ru>
2021-04-27 12:55:29 +0300

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,19 +11,17 @@ weatherForm.addEventListener("submit", (e) => {
 
   const location = search.value;
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          // console.log(data.error);
-          messageOne.textContent = data.error;
-        } else {
-          // console.log(data.location);
-          messageOne.textContent = data.location;
-          // console.log(data.forecast);
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        // console.log(data.error);
+        messageOne.textContent = data.error;
+      } else {
+        // console.log(data.location);
+        messageOne.textContent = data.location;
+        // console.log(data.forecast);
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
